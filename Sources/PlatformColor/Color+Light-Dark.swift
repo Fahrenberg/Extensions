b268a54b210e693f1dc31dd3c22960add9177f7e
Jean-Nicolas <70505484+Fahrenberg@users.noreply.github.com>
2025-02-22 11:48:38 +0100

    docs(Color+Light-Dark): Added function descriptions and references

--- Sources/PlatformColor/Color+Light-Dark.swift
+++ Sources/PlatformColor/Color+Light-Dark.swift
@@ -7,9 +7,16 @@
 import SwiftUI
 
 extension Color {
-    /// https://gist.github.com/delputnam/2d80e7b4bd9363fd221d131e4cfdbd8f
+    /// Proxy calculation if `Color is a light or dark color
+    ///
+    /// Calculates if foreground and background color combinations provide **not** sufficient contrast when viewed by someone having color deficits or when viewed on a black and white screen
+    ///
+    /// [GitHub Gist](https://gist.github.com/delputnam/2d80e7b4bd9363fd221d131e4cfdbd8f)
+    ///
+    /// [Used Algorithm](http://www.w3.org/WAI/ER/WD-AERT/#color-contrast)
+    ///
     public func isLight() -> Bool {
-        // algorithm from: http://www.w3.org/WAI/ER/WD-AERT/#color-contrast
+       
         guard let (r, g, b, _) = self.colorComponents else {
             return true
         }
@@ -20,6 +27,7 @@ extension Color {
 }
 
 extension ColorScheme  {
+    /// Selected color scheme for device
     public var description: String {
         return self == .light ? "Light" : "Dark"
     }
