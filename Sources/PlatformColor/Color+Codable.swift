dab7d592286224f1f5146bd8c38a408714207b04
Jean-Nicolas <70505484+Fahrenberg@users.noreply.github.com>
2026-03-31 16:33:02 +0200

    fix - Color+Codable: Fix warning by wrapping Color in CodableColor and retaining RGB extraction helper
    BREAKING API: Color no longer conforms to Codable; use CodableColor (Color.codable) for encoding/decoding

--- Sources/PlatformColor/Color+Codable.swift
+++ Sources/PlatformColor/Color+Codable.swift
@@ -1,16 +1,14 @@
 // --------------------------------------------------------------------------------------
-// ---------------------- SwiftUI - Color - Codng Extensions ----------------------------
+// ---------------------- SwiftUI - Color - Coding Extensions ---------------------------
 // --------------------------------------------------------------------------------------
 
 // MARK: Platform specfic imports
 import Foundation
 import SwiftUI
 
-// http://brunowernimont.me/howtos/make-swiftui-color-codable
-// added availability
 @available(iOS 14.0, *)
 @available(macOS 11.0, *)
-extension Color: Codable {
+extension Color {
      var colorComponents: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat)? {
         var r: CGFloat = 0
         var g: CGFloat = 0
@@ -30,29 +28,47 @@ extension Color: Codable {
         
         return (r, g, b, a)
     }
-    
-    enum CodingKeys: String, CodingKey {
-        case red, green, blue
+}
+
+@available(iOS 14.0, *)
+@available(macOS 11.0, *)
+public struct CodableColor: Codable, Hashable, Sendable {
+    public var color: Color
+
+    public init(_ color: Color) {
+        self.color = color
     }
-    
+
+    enum CodingKeys: String, CodingKey { case red, green, blue, alpha }
+
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         let r = try container.decode(Double.self, forKey: .red)
         let g = try container.decode(Double.self, forKey: .green)
         let b = try container.decode(Double.self, forKey: .blue)
-        
-        self.init(red: r, green: g, blue: b)
+        let a = try container.decodeIfPresent(Double.self, forKey: .alpha) ?? 1.0
+        self.color = Color(red: r, green: g, blue: b, opacity: a)
     }
-    
+
     public func encode(to encoder: Encoder) throws {
-        guard let colorComponents = self.colorComponents else {
-            return
-        }
-        
         var container = encoder.container(keyedBy: CodingKeys.self)
-        
-        try container.encode(colorComponents.red, forKey: .red)
-        try container.encode(colorComponents.green, forKey: .green)
-        try container.encode(colorComponents.blue, forKey: .blue)
+        if let comps = color.colorComponents {
+            try container.encode(Double(comps.red), forKey: .red)
+            try container.encode(Double(comps.green), forKey: .green)
+            try container.encode(Double(comps.blue), forKey: .blue)
+            try container.encode(Double(comps.alpha), forKey: .alpha)
+        } else {
+            // Fallback: encode as opaque black if components are unavailable
+            try container.encode(0.0, forKey: .red)
+            try container.encode(0.0, forKey: .green)
+            try container.encode(0.0, forKey: .blue)
+            try container.encode(1.0, forKey: .alpha)
+        }
     }
 }
+public extension Color {
+    @available(iOS 14.0, *)
+    @available(macOS 11.0, *)
+    var codable: CodableColor { CodableColor(self) }
+}
+
